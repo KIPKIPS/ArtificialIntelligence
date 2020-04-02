50ff8253dbacb4013c0821d60dee8e4e49a34bb6
RECNEPS-KIPS <2432719656@qq.com>
2020-04-02 12:06:08 +0800

    实现标记的显示

--- Assets/AIDemo_Enemy_01/Scripts/Chest.cs
+++ Assets/AIDemo_Enemy_01/Scripts/Chest.cs
@@ -7,14 +7,15 @@ public class Chest : MonoBehaviour {
     public Transform canvas;
     // Start is called before the first frame update
     void Start() {
-        flag = Instantiate(Resources.Load<GameObject>("AIDemo_01/HP"));
+        flag = Instantiate(Resources.Load<GameObject>("AIDemo_01/FlagPos"));
         flag.transform.parent = canvas;
+        flag.transform.localScale=new Vector3(0.2f,0.2f,0.2f);
     }
 
     // Update is called once per frame
     void Update() {
         Vector3 screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
         //根据世界坐标深度改变偏移量
-        flag.transform.position = new Vector3(screenPos.x, screenPos.y + Mathf.Clamp(200 * (1f / transform.position.z), 30, 40), screenPos.z);
+        flag.transform.position = new Vector3(screenPos.x, screenPos.y + Mathf.Clamp(2000 * (1f / transform.position.z), 30, 40), screenPos.z);
     }
 }
